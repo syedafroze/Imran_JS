54806b6bfa022b9d0cfb75c9f42defd0b074a3e1
syedafroze <safroze112@gmail.com>
2020-09-18 09:51:48 +0530

    "updated file"

--- Functions.js
+++ Functions.js
@@ -1,16 +1,43 @@
 
-// function add(num,num2){ //function declaration
+// // function add(num,num2){ //function declaration
 
+// //   let c= num+num2 ;     ///function definition
+// //   return c;                
+// // }
+
+// let add = function(num,num2){ //function declaration
 //   let c= num+num2 ;     ///function definition
 //   return c;                
 // }
 
-let add = function(num,num2){ //function declaration
-  let c= num+num2 ;     ///function definition
-  return c;                
-}
 
+
+// //Arrow functions /fat arrow func
+
+// let add = (num,num2)=> {
+//       return num+num2;
+// }
+
+//NaN - Not a Number 
+//default parameters  
+let add  =(num=0,num2=0) =>  num+num2 ; //0
 
 let result = add(2,3);//function call 
 
-console.log(result);
+console.log(result); 
+
+
+
+//semantic versioning
+//React 16.10.0
+
+//library -limited  (javascript)
+
+//{} return 
+
+let square = x => x*x; 
+
+square(5);//functiona call
+
+
+
