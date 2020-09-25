04695ce3aafe183f3a962f4c3d11f8bbabc34359
syedafroze <safroze112@gmail.com>
2020-09-25 10:22:21 +0530

    Updated files

--- Functions.js
+++ Functions.js
@@ -20,11 +20,11 @@
 
 //NaN - Not a Number 
 //default parameters  
-let add  =(num=0,num2=0) =>  num+num2 ; //0
+// let add  =(num=0,num2=0) =>  num+num2 ; //0
 
-let result = add(2,3);//function call 
+// let result = add(2,3);//function call 
 
-console.log(result); //
+// console.log(result); //
 
 
 
@@ -35,9 +35,52 @@ console.log(result); //
 
 //{} return 
 
-let square = x => x*x; 
+// let square = x => x*x; 
 
-square(5);//functiona call
+// square(5);//functiona call
 
 
+//recursive function - function calling itself 
 
+//factorial 5! = 5*4*3*2*1 = 120
+//1!=1  0!=1
+//5*4*3*2*1 
+//(5)*(5-1)*(5-2)*(5-3)*(5-4)
+
+function factorial(num){
+
+  if(num==1){
+    return 1;
+  }
+  else{
+    return num*factorial(num-1);//5*fact(4)
+  }
+}
+//console.log(factorial(5));
+/*
+factorial(5)
+return 5*4*3*2*1=120;
+factorial(4)
+return 4*3*2*1
+factorial(3)
+return 3*2*1;
+factorial(2);
+return 2*1;
+factorial(1);
+return 1;
+*/
+
+//currying functions
+
+function mul(a){
+
+  return function(b){
+
+    return function(c){
+      console.log(a*b*c);
+    }
+  }
+}
+
+let res = mul(2)(3)(4);
+console.log(res);
---
+++ Scopes.js
@@ -0,0 +1,41 @@
+//scope 
+//availablility of variable in program
+
+// var num =6;
+
+//block scope ,local scope , global scope,functional scope 
+
+//local scope - var 
+
+
+// //block (let and const are block scope )
+// {
+//  let num =6;
+//  const  num2=9
+// }
+
+
+
+//console.log(num2);
+
+//function scope
+
+// function fun(){
+//   var num =9;
+//   let num2=10;
+//   const  num3=13;
+// }
+
+//local
+
+
+
+
+//Hoisiting
+//it moves declaration to the top/beginning  of programmer 
+"use strict"
+
+// a=10;
+// console.log(a);
+// var  a;
+
