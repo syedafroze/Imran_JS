6c3f874faeccf57c0f406bf41fa8711e489c7306
syedafroze <safroze112@gmail.com>
2020-10-01 10:00:48 +0530

    Updated Files

--- Map_Weakmap.js
+++ Map_Weakmap.js
@@ -14,12 +14,48 @@ Users[user1]=1;//added user1 as key to Users
 let user2={
   name:"imran",
   id:9,
-  position:"devOps" 
+  position:"devOps",
+ 
 }
 Users[user2]=2;//addding user2 as akey in Users 
 
 console.log(Users)
 
 //Objects cannot store multiple objects as a key in it .
+
+
+//-----------Weakmap
+// {
+//   let x={};
+  
+//   var map = new WeakMap();
+//   map.set(x,2);
+// }
+
+
+
+// console.log(map)
+
+//-------------Map-------------
+
+// let user1={
+//   name:"syed",
+//   id:8,
+//   position:"devOps" 
+// }
+
+// let user2={
+//   name:"syed",
+//   id:8,
+//   position:"devOps" 
+// }
+
+
+// let Users= new Map();
+
+
+// Users.set(user1,1).set(user2,2);
+
+// console.log(Users)
 
 
