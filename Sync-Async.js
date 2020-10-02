e71bb7a587caf96adbbeeb5d993f3a2d19c59d6b
syedafroze <safroze112@gmail.com>
2020-10-02 10:09:02 +0530

    updated files

---
+++ Sync-Async.js
@@ -0,0 +1,35 @@
+
+//synchrnous and asychronous 
+
+
+// function add(a,b){
+//   return a+b;
+// }
+// let a=8;
+// let b=9;
+// console.log("in process");
+// let res = add(a,b);
+// console.log("value is ",res)
+// console.log("done");
+
+
+//asynchronous 
+
+console.log("in process");
+
+let c=8;let d=10;
+
+//1000ms = 1sec
+
+setTimeout(()=>console.log("done"),2000);
+
+console.log("value is ",c+d);
+
+
+//-------setTimer-----------------
+setTimeout(()=>console.log("start"),)
+let i=0;
+let interval= setInterval(()=>console.log(i++),1000);
+
+
+setTimeout(()=>clearInterval(interval),5000);
